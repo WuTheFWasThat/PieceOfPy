8b275a6f609bdd69668a930c44a95e55273de1bb
Shaunak Kishore <skishore@mit.edu>
2013-03-17 21:48:47 -0700

    Add do_command endpoint

--- server.py
+++ server.py
@@ -1,8 +1,32 @@
 import cherrypy
+import random
 import sys
 
-class CakeServer:
-  pass
+slice_types = [3, 4, 5, 7, 8, 9, 10, 11]
+cake_slices = sum(type*[type] for type in slice_types, [])
+num_slices = 55
+
+class CakeGame(object):
+  def __init__(self):
+    self.players = {}
+    self.slices = random.sample(cake_slices, num_slices)
+
+  def add_player(self, name):
+    if name not in self.players:
+      self.players[name] = {
+        'points': 0
+        'slices': []
+      }
+      return '%s joined the game' % (name,)
+    return '%s is already playing'
+
+class CakeServer(object):
+  @cherrypy.expose
+  def do_command(self, command):
+    print command
+    return {
+      'result': command
+    }
 
 if not sys.flags.interactive:
   cherrypy.quickstart(CakeServer(), '/', 'server.conf')
