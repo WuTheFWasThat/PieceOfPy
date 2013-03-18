94288593a21697b39692186c1bf1a03ce3504ba1
Jeff Wu <wuthefwasthat@gmail.com>
2013-03-17 23:02:17 -0700

    table

--- static/script.js
+++ static/script.js
@@ -30,45 +30,69 @@ function submit_command() {
 
 function update_game_state(game_state) {
   $('#game_state').empty();
-  var row = $('<tr><tr>');
-  row.append($('<td></td>').html('<b>Player</b>'))
-  row.append($('<td></td>').html('<b>Points</b>'))
-  row.append($('<td></td>').html('<b>3</b>'))
-  row.append($('<td></td>').html('<b>4</b>'))
-  row.append($('<td></td>').html('<b>5</b>'))
-  row.append($('<td></td>').html('<b>7</b>'))
-  row.append($('<td></td>').html('<b>8</b>'))
-  row.append($('<td></td>').html('<b>9</b>'))
-  row.append($('<td></td>').html('<b>10</b>'))
-  row.append($('<td></td>').html('<b>11</b>'))
+  var row = $('<tr></tr>');
+  row.append($('<td></td>').html('<b>Player</b>'));
+  row.append($('<td></td>').html('<b>Points</b>'));
+  row.append($('<td></td>').html('<b>3</b>'));
+  row.append($('<td></td>').html('<b>4</b>'));
+  row.append($('<td></td>').html('<b>5</b>'));
+  row.append($('<td></td>').html('<b>7</b>'));
+  row.append($('<td></td>').html('<b>8</b>'));
+  row.append($('<td></td>').html('<b>9</b>'));
+  row.append($('<td></td>').html('<b>10</b>'));
+  row.append($('<td></td>').html('<b>11</b>'));
   $('#game_state').append(row);
 
-  for (k in game_state) {
-    var player = game_state
+  for (player in game_state) {
+    var slices = [null, null, null, 0, 0, 0, null, 0, 0, 0, 0, 0];
+    var player_state = game_state[player];
+    for (i in player_state.slices) {
+      slices[player_state.slices[i]] += 1;
+    }
+    var row = $('<tr></tr>');
+    row.append($('<td></td>').text(player));
+    row.append($('<td></td>').text(player_state.points));
+    for (i in slices) {
+      if (slices[i] !== null) {
+        if (slices[i] == 0) {
+          row.append($('<td></td>'))
+        } else {
+          row.append($('<td></td>').text(slices[i]))
+        }
+      }
+    }
+    $('#game_state').append(row);
   }
+}
+
+var colors = [null, null, null, '#FF99CC', '#FF3333', '#FFB266', null, '#FFFF99', '#00CC66', '#6666FF', '#990099', '']
+
+function draw_slice(position, type) {
+  var context = document.getElementById('drawing').getContext('2d');
+  var radius = $('#drawing').height() / 2;
+  context.font = "bold 12px sans-serif";
+  context.fillText(type, 100, 100);
+
+  console.log(colors[type])
+  context.fillStyle= colors[type];
+  context.strokeStyle= colors[type];
+  context.moveTo(radius,radius);
+  context.arc(radius,radius,radius, 2 * Math.PI * position / 11, 2 * Math.PI * (position + 1) / 11);
+  context.lineTo(radius,radius);
+  context.fill(); // or context.fill()
 }
 
 $(document).ready(function() {
   var game_state = {
-    'shaunak': {points: 10, pieces: [10, 11, 9, 3]},
-    'jeff': {points: 10, pieces: [10, 11, 9, 3]},
+    'shaunak': {points: 10, slices: [10, 11, 9, 3]},
+    'jeff': {points: 10, slices: [10, 11, 9, 3, 3, 7, 7,]},
   }
   update_game_state(game_state);
 
-  var colors = [null, null, null, '#FF99CC', '#FF3333']
-  var context = document.getElementById('drawing').getContext('2d');
-  var radius = $('#drawing').height() / 2;
-  var i = 0;
-  var amount = 3;
-
-  context.font = "bold 12px sans-serif";
-  context.fillText(amount, 200, 200);
-
-  context.fillStyle= colors[amount];
-  context.strokeStyle= colors[amount];
-  context.moveTo(radius,radius);
-  context.arc(radius,radius,radius, 2 * Math.PI * i / 11, 2 * Math.PI * (i + 1) / 11);
-  context.lineTo(radius,radius);
-  context.fill(); // or context.fill()
+  for (var i = 0; i < 12; i += 1) {
+    if (colors[i] !== null) {
+      draw_slice(i, i);
+    }
+  }
 
 });
